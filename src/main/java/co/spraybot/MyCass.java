e2bf60929c3a9c6999a6e58b24d10d849ca1ab16
Arti <avartiverma3001@gmail.com>
2023-08-28 01:59:29 +0530

    first

---
+++ src/main/java/co/spraybot/MyCass.java
@@ -0,0 +1,16 @@
+package co.spraybot;
+
+public class MyCass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int result = sum(2,3);
+      System.out.println("The sum of 2 and 3 is :"+result);
+	}
+	
+	public static int sum(int a, int b) {
+		int sum = a+b;
+		return sum;
+	}
+
+}
---
+++ src/test/java/co/spraybot/MyCassTest.java
@@ -0,0 +1,14 @@
+package co.spraybot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyCassTest {
+
+	@Test
+	void test() {
+        assertEquals(MyCass.sum(2,3), 5);
+	}
+
+}
